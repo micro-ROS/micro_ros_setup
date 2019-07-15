1d966adebff0bb6483d929ca1f7714ee5fa6b14f
Ingo Lütkebohle <ingo.luetkebohle@de.bosch.com>
2019-07-15 10:31:11 +0200

    Fix parameter order

--- micro_ros_setup/scripts/create_client_ws.sh
+++ micro_ros_setup/scripts/create_client_ws.sh
@@ -4,4 +4,4 @@
 ros2 run micro_ros_setup create_ws.sh $1 client_ros2_packages.txt client_uros_packages.repos
 
 # add appropriate colcon.meta
-cp $(ros2 pkg micro_ros_setup prefix)/config/client-colcon.meta $1/colcon.meta
+cp $(ros2 pkg prefix micro_ros_setup)/config/client-colcon.meta $1/colcon.meta
