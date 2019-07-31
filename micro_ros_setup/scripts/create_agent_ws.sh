d43117db8d26e5d5a626879418f6b5ca34cbceaf
Luetkebohle Ingo (CR/AEX3) <ingo.luetkebohle@de.bosch.com>
2019-07-31 15:14:19 +0200

    put colcon.meta into TARGETDIR

--- micro_ros_setup/scripts/create_agent_ws.sh
+++ micro_ros_setup/scripts/create_agent_ws.sh
@@ -13,7 +13,7 @@ fi
 ros2 run micro_ros_setup create_ws.sh src agent_ros2_packages.txt agent_uros_packages.repos
 
 # add appropriate colcon.meta
-cp $(ros2 pkg prefix micro_ros_setup)/config/agent-colcon.meta agent_ws/colcon.meta
+cp $(ros2 pkg prefix micro_ros_setup)/config/agent-colcon.meta $TARGETDIR/colcon.meta
 
 rosdep install --from-paths $TARGETDIR -i $TARGETDIR -y \
   --skip-keys="rosidl_typesupport_opensplice_c rosidl_typesupport_opensplice_cpp rmw_opensplice_cpp rmw_connext_cpp rosidl_typesupport_connext_c rosidl_typesupport_connext_cpp"
