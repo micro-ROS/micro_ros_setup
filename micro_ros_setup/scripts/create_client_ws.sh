6e34e8ea0069dfd5530935ff0661e5e9452040d7
Borja Outerelo <borjaouterelo@gmail.com>
2019-09-25 13:43:01 +0200

    Revert "Allow client ws creation for demos (#22)" (#23)
    This reverts commit d5b2030681ad4a317aaeb69a76b9a9347204abf1.

--- micro_ros_setup/scripts/create_client_ws.sh
+++ micro_ros_setup/scripts/create_client_ws.sh
@@ -2,19 +2,9 @@
 
 set -e
 set -o nounset
-set -o pipefail
-
-TARGETDIR=src
-
-if [ $# -gt 0 ]
-then
-    TARGETDIR=$1
-fi
-
-[ -d $TARGETDIR ] || mkdir $TARGETDIR
 
 # populate the workspace
-ros2 run micro_ros_setup create_ws.sh $TARGETDIR client_ros2_packages.txt client_uros_packages.repos
+ros2 run micro_ros_setup create_ws.sh $1 client_ros2_packages.txt client_uros_packages.repos
 
 # add appropriate colcon.meta
-cp $(ros2 pkg prefix micro_ros_setup)/config/client-colcon.meta $TARGETDIR/colcon.meta
+cp $(ros2 pkg prefix micro_ros_setup)/config/client-colcon.meta $1/colcon.meta
