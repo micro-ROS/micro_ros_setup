b3eaef162c55812c761f774500c7e2924166b1ff
Borja Outerelo <borjaouterelo@gmail.com>
2019-09-26 07:56:06 +0200

    Resolve dependencies in host ws creation.

--- micro_ros_setup/scripts/create_host_client_ws.sh
+++ micro_ros_setup/scripts/create_host_client_ws.sh
@@ -5,6 +5,7 @@ set -o nounset
 set -o pipefail
 
 TARGETDIR=src
+SKIP="microxrcedds_client microcdr rosidl_typesupport_connext_cpp rosidl_typesupport_connext_c rosidl_typesupport_opensplice_cpp rosidl_typesupport_opensplice_c rmw_connext_cpp rmw_opensplice_cpp"
 
 if [ $# -gt 0 ]
 then
@@ -18,3 +19,5 @@ ros2 run micro_ros_setup create_ws.sh $TARGETDIR client_ros2_packages.txt client
 
 # add appropriate colcon.meta
 cp $(ros2 pkg prefix micro_ros_setup)/config/client-host-colcon.meta $TARGETDIR/colcon.meta
+
+rosdep install -y --from-paths $TARGETDIR -i $TARGETDIR --skip-keys="$SKIP"
