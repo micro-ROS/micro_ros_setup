230de049323fb2ebe3874fe7ccb27e1852c5c314
Pablo Garrido <pablogs9@gmail.com>
2019-11-26 14:25:08 +0100

    Updated to dashing

--- micro_ros_setup/config/freertos/crazyflie21/create.sh
+++ micro_ros_setup/config/freertos/crazyflie21/create.sh
@@ -13,7 +13,7 @@ pushd $FW_TARGETDIR >/dev/null
     # Creating dev directory
     mkdir dev_ws
     ros2 run micro_ros_setup create_ws.sh dev_ws $PREFIX/config/dev_ros2_packages.txt  $PREFIX/config/dev_uros_packages.repos
-    rosdep install -y --from-paths dev_ws -i dev_ws --rosdistro crystal --skip-keys="$SKIP"
+    rosdep install -y --from-paths dev_ws -i dev_ws --rosdistro dashing --skip-keys="$SKIP"
 
     # Creating mcu directory
     mkdir mcu_ws
@@ -23,10 +23,9 @@ pushd $FW_TARGETDIR >/dev/null
     # ignore broken packages
     touch mcu_ws/ros2/rcl_logging/rcl_logging_log4cxx/COLCON_IGNORE
     touch mcu_ws/ros2/rcl/rcl_action/COLCON_IGNORE
-    touch mcu_ws/ros2/rcutils/COLCON_IGNORE
 
     # Update deps
-    # rosdep install -y --from-paths mcu_ws -i mcu_ws --rosdistro crystal --skip-keys="$SKIP"
+    rosdep install -y --from-paths mcu_ws -i mcu_ws --rosdistro dashing --skip-keys="$SKIP"
 
     # Turn off features MicroXRCEClient
     echo -e ",s/PROFILE_DISCOVERY=TRUE/PROFILE_DISCOVERY=FALSE/\n,s/PROFILE_UDP_TRANSPORT=TRUE/PROFILE_UDP_TRANSPORT=FALSE/\n,s/PROFILE_TCP_TRANSPORT=TRUE/PROFILE_TCP_TRANSPORT=FALSE/g\nw" | ed $(find mcu_ws -name client.config) >/dev/null &>/dev/null
