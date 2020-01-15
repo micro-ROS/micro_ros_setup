879be8f32dcf2c2542c1be54c5cbe515fa830d89
Pablo Garrido <pablogs9@gmail.com>
2020-01-15 10:56:57 +0100

    Fix create and build scripts

--- micro_ros_setup/config/nuttx/olimex-stm32-e407/create.sh
+++ micro_ros_setup/config/nuttx/olimex-stm32-e407/create.sh
@@ -1,4 +1,4 @@
-apt install -y gcc-arm-none-eabi clang clang-tidy
+sudo apt install -y gcc-arm-none-eabi clang clang-tidy
 
 [ -d $FW_TARGETDIR ] || mkdir $FW_TARGETDIR
 pushd $FW_TARGETDIR >/dev/null
@@ -16,6 +16,8 @@ pushd $FW_TARGETDIR >/dev/null
     # ignore broken packages
     touch mcu_ws/ros2/rcl_logging/rcl_logging_log4cxx/COLCON_IGNORE
     touch mcu_ws/ros2/rcl/rcl_action/COLCON_IGNORE
+
+    rosdep install -y --from-paths mcu_ws -i mcu_ws --rosdistro dashing --skip-keys="$SKIP"
 
     # turn off features which don't compile on NuttX currently
     echo -e ",s/PROFILE_DISCOVERY=TRUE/PROFILE_DISCOVERY=FALSE/\n,s/PROFILE_TCP_TRANSPORT=TRUE/PROFILE_TCP_TRANSPORT=FALSE/g\nw" | ed $(find mcu_ws -name client.config) >/dev/null
