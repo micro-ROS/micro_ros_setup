6ecf5ac37226929a6eaf244eb748d640aab5107b
Pablo Garrido <pablogs9@gmail.com>
2020-01-29 08:33:09 +0100

    Fix fast build

--- micro_ros_setup/config/freertos/olimex_e407/build.sh
+++ micro_ros_setup/config/freertos/olimex_e407/build.sh
@@ -1,7 +1,7 @@
 OLIMEX_EXTENSIONS_DIR=$FW_TARGETDIR/olimex_e407_extensions
 
 pushd $OLIMEX_EXTENSIONS_DIR >/dev/null
-    if [ "$UROS_FAST_BUILD" = "off" ] || [ ! -d "bin" ]; then
+    if [ "$UROS_FAST_BUILD" = "off" ] || [ ! -d "build" ]; then
         # clean build
         make clean
 
