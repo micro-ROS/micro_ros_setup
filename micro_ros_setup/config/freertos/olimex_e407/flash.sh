49ff03c3c5bbb8bd436a2f21e6674d78a2e3d3cd
Pablo Garrido <pablogs9@gmail.com>
2020-01-29 09:24:27 +0100

    Supporting multiple programmers

--- micro_ros_setup/config/freertos/olimex_e407/flash.sh
+++ micro_ros_setup/config/freertos/olimex_e407/flash.sh
@@ -4,7 +4,19 @@ pushd $OLIMEX_EXTENSIONS_DIR > /dev/null
 
   if [ -f build/micro-ROS.bin ]; then
     echo "Flashing firmware for $RTOS platform $PLATFORM"
-    openocd -f interface/ftdi/olimex-arm-usb-tiny-h.cfg -f target/stm32f4x.cfg -c init -c "reset halt" -c "flash write_image erase build/micro-ROS.bin 0x08000000" -c "reset" -c "exit"
+
+      if lsusb -d 15BA:002a; then
+        PROGRAMMER=interface/ftdi/olimex-arm-usb-tiny-h.cfg
+      elif lsusb -d 15BA:0003;then
+        PROGRAMMER=interface/ftdi/olimex-arm-usb-ocd.cfg
+      elif lsusb -d 15BA:002b;then
+        PROGRAMMER=interface/ftdi/olimex-arm-usb-ocd-h.cfg
+      else
+        echo "Error. Unsuported OpenOCD USB programmer"
+        exit 1
+      fi
+
+      openocd -f $PROGRAMMER -f target/stm32f4x.cfg -c init -c "reset halt" -c "flash write_image erase build/micro-ROS.bin 0x08000000" -c "reset" -c "exit"
   else
     echo "build/micro-ROS.bin not found: please compile before flashing."
   fi
