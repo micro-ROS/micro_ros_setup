292d12e619d42fe4ac8a5cfbc91b6cee43e32d52
Pablo Garrido <pablogs9@gmail.com>
2020-01-29 08:36:00 +0100

    Exit after flash

--- micro_ros_setup/config/freertos/olimex_e407/flash.sh
+++ micro_ros_setup/config/freertos/olimex_e407/flash.sh
@@ -4,7 +4,7 @@ pushd $OLIMEX_EXTENSIONS_DIR > /dev/null
 
   if [ -f build/micro-ROS.bin ]; then
     echo "Flashing firmware for $RTOS platform $PLATFORM"
-    openocd -f interface/ftdi/olimex-arm-usb-tiny-h.cfg -f target/stm32f4x.cfg -c init -c "reset halt" -c "flash write_image erase build/micro-ROS.bin 0x08000000" -c "reset"
+    openocd -f interface/ftdi/olimex-arm-usb-tiny-h.cfg -f target/stm32f4x.cfg -c init -c "reset halt" -c "flash write_image erase build/micro-ROS.bin 0x08000000" -c "reset" -c "exit"
   else
     echo "build/micro-ROS.bin not found: please compile before flashing."
   fi
