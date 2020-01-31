f47618145ee4a23d9164260c2079eb4de77b7d9d
Pablo Garrido <pablogs9@gmail.com>
2020-01-31 10:51:07 +0100

    Updated configure script with serial

--- micro_ros_setup/config/freertos/olimex_e407/configure.sh
+++ micro_ros_setup/config/freertos/olimex_e407/configure.sh
@@ -1,12 +1,18 @@
 
-function add_to_meta {
-      python3 -c "import sys; import json; c = '$ARG_TO_ADD'; s = json.loads(''.join([l for l in sys.stdin])); k = s['names']['rmw_microxrcedds']['cmake-args']; i = [c.startswith(v.split('=')[0]) for v in k]; k.pop(i.index(True)) if any(i) else None; k.append(c); print(json.dumps(s,indent=4))" < $FW_TARGETDIR/mcu_ws/colcon.meta > $FW_TARGETDIR/mcu_ws/colcon_new.meta
+OLIMEX_EXTENSIONS_DIR=$FW_TARGETDIR/olimex_e407_extensions
+
+function update_meta {
+      python3 -c "import sys; import json; c = '$2'; s = json.loads(''.join([l for l in sys.stdin])); k = s['names']['$1']['cmake-args']; i = [c.startswith(v.split('=')[0]) for v in k]; k.pop(i.index(True)) if any(i) else None; k.append(c) if len(c.split('=')[1]) else None; print(json.dumps(s,indent=4))" < $FW_TARGETDIR/mcu_ws/colcon.meta > $FW_TARGETDIR/mcu_ws/colcon_new.meta
       mv $FW_TARGETDIR/mcu_ws/colcon_new.meta $FW_TARGETDIR/mcu_ws/colcon.meta
 }
 
+function help {
+      echo "Configure script need an argument. For example: ros2 run micro_ros_setup configure_firmware.sh [udp | tcp | serial] [IP address | Serial port] [IP port]"
+}
+
 if [ $# -lt 1 ]; then
-    echo "Configure script need an argument. For example: ros2 run micro_ros_setup configure_firmware.sh [udp | tcp | serial] [IP address | Serial port] [IP port]"
-    exit 1
+      help
+      exit 1
 fi
 
 TRANSPORT=$1
@@ -16,36 +22,36 @@ if [ "$TRANSPORT" == "udp" ] || [ "$TRANSPORT" == "tcp" ]; then
             echo "UDP or TCP configuration needs IP and port. For example: ros2 run micro_ros_setup configure_firmware.sh [udp | tcp] [IP address] [IP port]"
             exit 1
       fi
-      
-      ARG_TO_ADD="-DRMW_UXRCE_TRANSPORT="$TRANSPORT
-      add_to_meta
 
       IP=$2
-      ARG_TO_ADD="-DRMW_UXRCE_DEFAULT_UDP_IP="$IP
-      add_to_meta
-
       PORT=$3
-      ARG_TO_ADD="-DRMW_UXRCE_DEFAULT_UDP_PORT="$PORT
-      add_to_meta
+
+      update_meta "rmw_microxrcedds" "-DRMW_UXRCE_TRANSPORT="$TRANSPORT
+      update_meta "rmw_microxrcedds" "-DRMW_UXRCE_DEFAULT_UDP_IP="$IP
+      update_meta "rmw_microxrcedds" "-DRMW_UXRCE_DEFAULT_UDP_PORT="$PORT
+
+      update_meta "microxrcedds_client" "-DEXTERNAL_TRANSPORT_HEADER_SERIAL="
+      update_meta "microxrcedds_client" "-DEXTERNAL_TRANSPORT_SRC_SERIAL="
 
       echo "Configured $TRANSPORT mode with agent at $IP:$PORT"
 
-fi
-
-if [ "$TRANSPORT" == "serial" ]; then
-      echo "Serial configuration not working yet. Work in Progress"
-      exit 1
+elif [ "$TRANSPORT" == "serial" ]; then
       if [ $# -lt 2 ]; then
-            echo "Serial configuration needs serial port. For example: ros2 run micro_ros_setup configure_firmware.sh serial [Serial port]"
-            exit 1
+            SERIAL="3"
+      else
+            SERIAL=$2
       fi
+      echo "Using serial device USART$SERIAL."
 
-      ARG_TO_ADD="-DRMW_UXRCE_TRANSPORT="$TRANSPORT
-      add_to_meta
+      update_meta "rmw_microxrcedds" "-DRMW_UXRCE_TRANSPORT=custom"
+      update_meta "rmw_microxrcedds" "-DRMW_UXRCE_DEFAULT_SERIAL_DEVICE="$SERIAL
+      update_meta "microxrcedds_client" "-DEXTERNAL_TRANSPORT_HEADER_SERIAL="$OLIMEX_EXTENSIONS_DIR"/Inc/olimex_e407_serial_transport.h"
+      update_meta "microxrcedds_client" "-DEXTERNAL_TRANSPORT_SRC_SERIAL="$OLIMEX_EXTENSIONS_DIR"/Src/olimex_e407_serial_transport.c"
 
-      SERIAL=$2
-      ARG_TO_ADD="-DRMW_UXRCE_DEFAULT_SERIAL_DEVICE="$SERIAL
-      add_to_meta
+      update_meta "rmw_microxrcedds" "-DRMW_UXRCE_DEFAULT_UDP_IP="
+      update_meta "rmw_microxrcedds" "-DRMW_UXRCE_DEFAULT_UDP_PORT="
 
-      echo "Configured $TRANSPORT mode with agent at $SERIAL"
+      echo "Configured $TRANSPORT mode with agent at USART$SERIAL"
+else
+      help
 fi
