d027b0e70f37b22601f0701f91f85ae5fca17298
Pablo Garrido <pablogs9@gmail.com>
2020-02-26 11:03:41 +0100

    path fix

--- micro_ros_setup/config/zephyr/olimex-stm32-e407/build.sh
+++ micro_ros_setup/config/zephyr/olimex-stm32-e407/build.sh
@@ -1,7 +1,9 @@
 pushd $FW_TARGETDIR >/dev/null
+    source $FW_TARGETDIR/zephyrproject/zephyr/zephyr-env.sh
     export ZEPHYR_TOOLCHAIN_VARIANT=zephyr
     export ZEPHYR_SDK_INSTALL_DIR=$(pwd)/zephyr-sdk-0.11.1
-    source $FW_TARGETDIR/zephyrproject/zephyr/zephyr-env.sh
+
+    export PATH=~/.local/bin:"$PATH"
 
     rm -rf build
     
