1fb6abdcd655eaf113527ec6321ef42a1fed6ffe
Pablo Garrido <pablogs9@gmail.com>
2020-03-13 11:27:03 +0100

    Using ROS2 ros_idl

--- micro_ros_setup/config/zephyr/generic/create.sh
+++ micro_ros_setup/config/zephyr/generic/create.sh
@@ -44,7 +44,6 @@ pushd $FW_TARGETDIR >/dev/null
     # ignore broken packages
     touch mcu_ws/ros2/rcl_logging/rcl_logging_log4cxx/COLCON_IGNORE
     touch mcu_ws/ros2/rcl/COLCON_IGNORE
-    touch mcu_ws/ros2/rosidl_typesupport/COLCON_IGNORE
 
     rosdep install -y --from-paths mcu_ws -i mcu_ws --rosdistro dashing --skip-keys="$SKIP"
 popd >/dev/null
