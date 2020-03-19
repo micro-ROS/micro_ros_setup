0ab81132836d99bb2fe527aadc69f769961e2bb7
Pablo Garrido <pablogs9@gmail.com>
2020-03-19 13:21:33 +0100

    Update zephyr Flash to Olimex support (#88)
    * Update zephyr flash to olimex support
    
    * Fix CI for the new changes on Zephyr (#89)
    
    * Fix CI for the new changes on Zephyr
    
    * Fix freeRTOS issue with new apps
    
    * Update rmw branch
    
    * Update rmw_microxrcedds crazyflie
    
    Co-authored-by: Juan Flores Muñoz <juan@erlerobotics.com>

--- micro_ros_setup/config/zephyr/generic/flash.sh
+++ micro_ros_setup/config/zephyr/generic/flash.sh
@@ -1,5 +1,6 @@
 pushd $FW_TARGETDIR > /dev/null
 
+if [ "$PLATFORM" = "discovery_l475_iot1" ]; then
   export ZEPHYR_TOOLCHAIN_VARIANT=zephyr
   export ZEPHYR_SDK_INSTALL_DIR=$FW_TARGETDIR/zephyr-sdk
 
@@ -8,6 +9,29 @@ pushd $FW_TARGETDIR > /dev/null
   export PATH=~/.local/bin:"$PATH"
 
   west flash
+elif [ "$PLATFORM" = "olimex-stm32-e407" ]; then
+  if [ -f build/zephyr/zephyr.bin ]; then
+    echo "Flashing firmware for $RTOS platform $PLATFORM"
+
+      if lsusb -d 15BA:002a; then
+        PROGRAMMER=interface/ftdi/olimex-arm-usb-tiny-h.cfg
+      elif lsusb -d 15BA:0003;then
+        PROGRAMMER=interface/ftdi/olimex-arm-usb-ocd.cfg
+      elif lsusb -d 15BA:002b;then
+        PROGRAMMER=interface/ftdi/olimex-arm-usb-ocd-h.cfg
+      else
+        echo "Error. Unsuported OpenOCD USB programmer"
+        exit 1
+      fi
+
+      openocd -f $PROGRAMMER -f target/stm32f4x.cfg -c init -c "reset halt" -c "flash write_image erase build/zephyr/zephyr.bin 0x08000000" -c "reset" -c "exit"
+  else
+    echo "build/zephyr/zephyr.bin not found: please compile before flashing."
+  fi
+else
+  echo "Unrecognized board: $PLATFORM"
+  exit 1
+fi
 
 popd > /dev/null
 
