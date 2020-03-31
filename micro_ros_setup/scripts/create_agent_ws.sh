52eab0cf3753f5174f0c35cbff327e14bf23a058
Pablo Garrido <pablogs9@gmail.com>
2020-03-31 15:55:02 +0200

    Fix

--- micro_ros_setup/scripts/create_agent_ws.sh
+++ micro_ros_setup/scripts/create_agent_ws.sh
@@ -6,7 +6,7 @@ set -o pipefail
 
 PREFIX=$(ros2 pkg prefix micro_ros_setup)
 TARGETDIR=src
-SKIP="rosidl_typesupport_opensplice_c rosidl_typesupport_opensplice_cpp rmw_opensplice_cpp rmw_connext_cpp rosidl_typesupport_connext_c rosidl_typesupport_connext_cpp microxrcedds_agent"
+SKIP="microcdr microxrcedds_client rosidl_typesupport_opensplice_c rosidl_typesupport_opensplice_cpp rmw_opensplice_cpp rmw_connext_cpp rosidl_typesupport_connext_c rosidl_typesupport_connext_cpp microxrcedds_agent"
 
 if [ $# -gt 0 ]
 then
