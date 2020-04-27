d62156e7440fdd4058f07e68b0bfc0d4f651c898
Julián Bermúdez Ortega <julianbermudez@eprosima.com>
2020-04-27 10:12:51 +0200

    Raspbian config (#109)
    * Add raspbian config core.
    
    * Implement raspbian's create.sh script.
    
    * Implement raspbian's configure.sh script.
    
    * Implement raspbian's build.sh script.
    
    * Modify build.sh and configure.sh script for raspbian.
    
    * Implement app_info.sh handling for raspbian.
    
    * Change raspbian_apps repository url.
    
    * Add flash.sh script for raspbian.
    
    * Include raspbian in CI.
    
    * Remove toolchain.cmake copy during create.sh script for raspbian.

---
+++ micro_ros_setup/config/raspbian/generic/build.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+pushd $FW_TARGETDIR/mcu_ws >/dev/null
+    source app_info.sh
+    PATH=$PWD/../dev_ws/xcompiler/bin:$PATH
+    LD_LIBRARY_PATH=$PWD/../dev_ws/xcompiler/lib:$PWD/../dev_ws/xcompiler/arm-linux-gnueabihf/lib
+    colcon build --packages-up-to=$APP_PACKAGE_NAME --merge-install --cmake-force-configure \
+        --cmake-args \
+            -DCMAKE_TOOLCHAIN_FILE=$PWD/toolchain.cmake \
+            -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
+            -DBUILD_SHARED_LIBS=OFF \
+            -DBUILD_TESTING=OFF
+    find ./install -executable -type f -name $APP_BINARY_NAME -exec cp {} $FW_TARGETDIR/bin/$APP_OUTPUT_NAME \;
+popd >/dev/null
