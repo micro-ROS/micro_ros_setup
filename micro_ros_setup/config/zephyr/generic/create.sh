cfe6685bd86b0dc85f7d2d22b9a3446d23c7e1cb
Pablo Garrido <pablogs9@gmail.com>
2020-06-02 13:45:27 +0200

    Avoid multiple typesupports

--- micro_ros_setup/config/zephyr/generic/create.sh
+++ micro_ros_setup/config/zephyr/generic/create.sh
@@ -52,6 +52,8 @@ pushd $FW_TARGETDIR >/dev/null
     touch mcu_ws/ros2/rcl_logging/rcl_logging_log4cxx/COLCON_IGNORE
     touch mcu_ws/ros2/rcl_logging/rcl_logging_spdlog/COLCON_IGNORE
     touch mcu_ws/ros2/rcl/COLCON_IGNORE
+    touch mcu_ws/ros2/rosidl/rosidl_typesupport_introspection_c/COLCON_IGNORE
+    touch mcu_ws/ros2/rosidl/rosidl_typesupport_introspection_cpp/COLCON_IGNORE
 
     rosdep install -y --from-paths mcu_ws -i mcu_ws --rosdistro dashing --skip-keys="$SKIP"
 popd >/dev/null
