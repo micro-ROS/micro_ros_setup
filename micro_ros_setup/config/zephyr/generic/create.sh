69c4b6ee136783f61cfc719109fe7d17f30e0d6f
julibert <julianbermudez@eprosima.com>
2020-06-04 13:42:46 +0000

    Ignore rcpputils and rclc_examples for Zephyr.

--- micro_ros_setup/config/zephyr/generic/create.sh
+++ micro_ros_setup/config/zephyr/generic/create.sh
@@ -45,6 +45,8 @@ pushd $FW_TARGETDIR >/dev/null
     touch mcu_ws/ros2/rcl/COLCON_IGNORE
     touch mcu_ws/ros2/rosidl/rosidl_typesupport_introspection_c/COLCON_IGNORE
     touch mcu_ws/ros2/rosidl/rosidl_typesupport_introspection_cpp/COLCON_IGNORE
+    touch mcu_ws/ros2/rcpputils/COLCON_IGNORE
+    touch mcu_ws/uros/rclc/rclc_examples/COLCON_IGNORE
 
     rosdep install -y --from-paths mcu_ws -i mcu_ws --rosdistro foxy --skip-keys="$SKIP"
 popd >/dev/null
