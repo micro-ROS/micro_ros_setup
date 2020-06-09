8df08574ad6f2aba44326edfbf5cc37f70e596bf
Pablo Garrido <pablogs9@gmail.com>
2020-06-09 07:17:53 +0200

    Updated rclc branches

--- micro_ros_setup/config/host/generic/create.sh
+++ micro_ros_setup/config/host/generic/create.sh
@@ -6,6 +6,4 @@ cp $PREFIX/config/$RTOS/$PLATFORM/client-host-colcon.meta src/colcon.meta
 
 rosdep install -y --from-paths src -i src --skip-keys="$SKIP"
 
-# TODO (pablogs9): Remove when rclc builds correctly
-touch src/uros/micro-ROS-demos/rclc/COLCON_IGNORE
-touch src/uros/rclc/COLCON_IGNORE
+touch mcu_ws/uros/rclc/rclc_examples/COLCON_IGNORE
