431d80fc0a6aff16190e563aaba55402e10fdcad
Pablo Garrido <pablogs9@gmail.com>
2020-06-12 10:51:22 +0200

    Updated freertos bs

--- micro_ros_setup/config/freertos/olimex-stm32-e407/create.sh
+++ micro_ros_setup/config/freertos/olimex-stm32-e407/create.sh
@@ -16,6 +16,11 @@ pushd $FW_TARGETDIR >/dev/null
     touch mcu_ws/ros2/rcl_logging/rcl_logging_log4cxx/COLCON_IGNORE
     touch mcu_ws/ros2/rcl_logging/rcl_logging_spdlog/COLCON_IGNORE
     touch mcu_ws/ros2/rcl/COLCON_IGNORE
+    touch mcu_ws/ros2/rosidl/rosidl_typesupport_introspection_c/COLCON_IGNORE
+    touch mcu_ws/ros2/rosidl/rosidl_typesupport_introspection_cpp/COLCON_IGNORE
+    touch mcu_ws/ros2/rcpputils/COLCON_IGNORE
+    touch mcu_ws/uros/rcl/rcl_yaml_param_parser/COLCON_IGNORE
+    touch mcu_ws/uros/rclc/rclc_examples/COLCON_IGNORE
 
     rosdep install -y --from-paths mcu_ws -i mcu_ws --rosdistro foxy --skip-keys="$SKIP"
 popd >/dev/null
