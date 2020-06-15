3a1645443beb98fbdc49b345fa1f5d9a26d8d28f
Pablo Garrido <pablogs9@gmail.com>
2020-06-15 07:40:44 +0200

    Transport profile selection

--- micro_ros_setup/config/freertos/olimex-stm32-e407/configure.sh
+++ micro_ros_setup/config/freertos/olimex-stm32-e407/configure.sh
@@ -18,8 +18,12 @@ if [ "$UROS_TRANSPORT" == "udp" ] || [ "$UROS_TRANSPORT" == "tcp" ]; then
       update_meta "rmw_microxrcedds" "RMW_UXRCE_TRANSPORT="$UROS_TRANSPORT
       update_meta "rmw_microxrcedds" "RMW_UXRCE_DEFAULT_UDP_IP="$UROS_AGENT_IP
       update_meta "rmw_microxrcedds" "RMW_UXRCE_DEFAULT_UDP_PORT="$UROS_AGENT_PORT
-
+      update_meta "microxrcedds_client" "UCLIENT_PROFILE_SERIAL=OFF"
+      update_meta "microxrcedds_client" "UCLIENT_PROFILE_UDP=ON"
+      update_meta "microxrcedds_client" "UCLIENT_PROFILE_TCP=OFF"
+      
       remove_meta "rmw_microxrcedds" "RMW_UXRCE_DEFAULT_SERIAL_DEVICE"
+      remove_meta "microxrcedds_client" "UCLIENT_EXTERNAL_SERIAL"
       remove_meta "microxrcedds_client" "EXTERNAL_TRANSPORT_HEADER_SERIAL"
       remove_meta "microxrcedds_client" "EXTERNAL_TRANSPORT_SRC_SERIAL"
 
@@ -31,6 +35,9 @@ elif [ "$UROS_TRANSPORT" == "serial" ]; then
       cp -f $EXTENSIONS_DIR/Src/olimex_e407_serial_transport.c $FW_TARGETDIR/mcu_ws/eProsima/Micro-XRCE-DDS-Client/src/c/profile/transport/serial/serial_transport_external.c
       cp -f $EXTENSIONS_DIR/Inc/olimex_e407_serial_transport.h $FW_TARGETDIR/mcu_ws/eProsima/Micro-XRCE-DDS-Client/include/uxr/client/profile/transport/serial/serial_transport_external.h
       update_meta "microxrcedds_client" "UCLIENT_EXTERNAL_SERIAL=ON"
+      update_meta "microxrcedds_client" "UCLIENT_PROFILE_SERIAL=ON"
+      update_meta "microxrcedds_client" "UCLIENT_PROFILE_UDP=OFF"
+      update_meta "microxrcedds_client" "UCLIENT_PROFILE_TCP=OFF"
 
       update_meta "rmw_microxrcedds" "RMW_UXRCE_TRANSPORT=custom_serial"
       update_meta "rmw_microxrcedds" "RMW_UXRCE_DEFAULT_SERIAL_DEVICE="$UROS_AGENT_DEVICE
