d2227d2c194dcb60a9b7040caec29647421fc6fe
alsaibie <alsaibie@gmail.com>
2020-06-22 21:19:39 +0300

    switched to stlink flash util - working

--- micro_ros_setup/config/freertos/nucleo_f446ze/flash.sh
+++ micro_ros_setup/config/freertos/nucleo_f446ze/flash.sh
@@ -1,26 +1,23 @@
-OLIMEX_EXTENSIONS_DIR=$FW_TARGETDIR/freertos_apps/microros_nucleo_f446ze_extensions
+NUCLEO_EXTENSIONS_DIR=$FW_TARGETDIR/freertos_apps/microros_nucleo_f446ze_extensions
 
-pushd $OLIMEX_EXTENSIONS_DIR > /dev/null
+pushd $NUCLEO_EXTENSIONS_DIR > /dev/null
 
 #TODO: fix into st-link
 
-  if [ -f build/micro-ROS.bin ]; then
+  if [ -f build/nucleof446ze_uros_template.bin ]; then
     echo "Flashing firmware for $RTOS platform $PLATFORM"
 
-      if lsusb -d 15BA:002a; then
-        PROGRAMMER=interface/ftdi/olimex-arm-usb-tiny-h.cfg
-      elif lsusb -d 15BA:0003;then
-        PROGRAMMER=interface/ftdi/olimex-arm-usb-ocd.cfg
-      elif lsusb -d 15BA:002b;then
-        PROGRAMMER=interface/ftdi/olimex-arm-usb-ocd-h.cfg
+      if lsusb -d 0483:374b; then
+        PROGRAMMER=interface/stlink-v2.cfg
       else
         echo "Error. Unsuported OpenOCD USB programmer"
         exit 1
       fi
 
-      openocd -f $PROGRAMMER -f target/stm32f4x.cfg -c init -c "reset halt" -c "flash write_image erase build/micro-ROS.bin 0x08000000" -c "reset" -c "exit"
+     # openocd -f $PROGRAMMER -f target/stm32f4x.cfg -c init -c "reset halt" -c "flash write_image erase build/nucleof446ze_uros_template.bin 0x08000000" -c "reset" -c "exit"
+     st-flash --reset write build/nucleof446ze_uros_template.bin 0x8000000
   else
-    echo "build/micro-ROS.bin not found: please compile before flashing."
+    echo "build/nucleof446ze_uros_template.bin not found: please compile before flashing."
   fi
 
 popd > /dev/null
