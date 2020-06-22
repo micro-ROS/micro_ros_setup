f6dfe9c5b37e38aa4003b2ec9f680443b5cf0d99
alsaibie <alsaibie@gmail.com>
2020-06-22 10:35:34 +0300

    added nucleo_f446ze configuration

---
+++ micro_ros_setup/config/freertos/nucleo_f446ze/configure.sh
@@ -0,0 +1,34 @@
+
+EXTENSIONS_DIR=$FW_TARGETDIR/freertos_apps/microros_nucleo_f446ze_extensions
+
+. $PREFIX/config/utils.sh
+
+function help {
+      echo "Configure script need an argument."
+     # echo "   --transport -t       udp, tcp, serial or serial-usb"
+     echo "   --transport -t        serial or serial-usb"
+      echo "   --dev -d             agent string descriptor in a serial-like transport"
+     # echo "   --ip -i              agent IP in a network-like transport"
+     # echo "   --port -p            agent port in a network-like transport"
+}
+
+echo $CONFIG_NAME > $FW_TARGETDIR/APP
+
+if [ "$UROS_TRANSPORT" == "serial" ]; then
+      echo "Using serial device USART$UROS_AGENT_DEVICE."
+
+      cp -f $EXTENSIONS_DIR/Src/stm32f446ze_serial_transport.c $FW_TARGETDIR/mcu_ws/eProsima/Micro-XRCE-DDS-Client/src/c/profile/transport/serial/serial_transport_external.c
+      cp -f $EXTENSIONS_DIR/Inc/stm32f446ze_serial_transport.h $FW_TARGETDIR/mcu_ws/eProsima/Micro-XRCE-DDS-Client/include/uxr/client/profile/transport/serial/serial_transport_external.h
+      update_meta "microxrcedds_client" "UCLIENT_EXTERNAL_SERIAL=ON"
+
+      update_meta "rmw_microxrcedds" "RMW_UXRCE_TRANSPORT=custom"
+      update_meta "rmw_microxrcedds" "RMW_UXRCE_DEFAULT_SERIAL_DEVICE="$UROS_AGENT_DEVICE
+
+      remove_meta "rmw_microxrcedds" "RMW_UXRCE_DEFAULT_UDP_IP"
+      remove_meta "rmw_microxrcedds" "RMW_UXRCE_DEFAULT_UDP_PORT"
+
+      echo "Configured $UROS_TRANSPORT mode with agent at USART$UROS_AGENT_DEVICE"
+
+else
+      help
+fi
