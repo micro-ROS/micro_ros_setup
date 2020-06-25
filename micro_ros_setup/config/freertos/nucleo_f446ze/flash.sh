60494c3608ff9ea97f04b48a5c5606a962d9b300
alsaibie <alsaibie@gmail.com>
2020-06-25 11:49:02 +0300

    chmod on bash scripts and removed openocd flash, replaced with st-flash

--- micro_ros_setup/config/freertos/nucleo_f446ze/flash.sh
+++ micro_ros_setup/config/freertos/nucleo_f446ze/flash.sh
@@ -6,16 +6,7 @@ pushd $NUCLEO_EXTENSIONS_DIR > /dev/null
 
   if [ -f build/nucleof446ze_uros_template.bin ]; then
     echo "Flashing firmware for $RTOS platform $PLATFORM"
-
-      if lsusb -d 0483:374b; then
-        PROGRAMMER=interface/stlink-v2.cfg
-      else
-        echo "Error. Unsuported OpenOCD USB programmer"
-        exit 1
-      fi
-
-     # openocd -f $PROGRAMMER -f target/stm32f4x.cfg -c init -c "reset halt" -c "flash write_image erase build/nucleof446ze_uros_template.bin 0x08000000" -c "reset" -c "exit"
-     st-flash --reset write build/nucleof446ze_uros_template.bin 0x8000000
+    st-flash --reset write build/nucleof446ze_uros_template.bin 0x8000000
   else
     echo "build/nucleof446ze_uros_template.bin not found: please compile before flashing."
   fi
