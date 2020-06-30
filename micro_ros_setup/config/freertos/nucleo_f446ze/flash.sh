0eea0fbbe36cafb4ead89292163908c1b7eca727
alsaibie <alsaibie@gmail.com>
2020-06-30 10:45:45 +0300

    cleanup port, added back openocd flash

--- micro_ros_setup/config/freertos/nucleo_f446ze/flash.sh
+++ micro_ros_setup/config/freertos/nucleo_f446ze/flash.sh
@@ -1,14 +1,28 @@
-NUCLEO_EXTENSIONS_DIR=$FW_TARGETDIR/freertos_apps/microros_nucleo_f446ze_extensions
+EXTENSIONS_DIR=$FW_TARGETDIR/freertos_apps/microros_nucleo_f446ze_extensions
 
-pushd $NUCLEO_EXTENSIONS_DIR > /dev/null
+USE_STFLASH=false
 
-#TODO: fix into st-link
+pushd $EXTENSIONS_DIR > /dev/null
 
-  if [ -f build/nucleof446ze_uros_template.bin ]; then
+  if [ -f build/micro-ROS.bin ]; then
+    
     echo "Flashing firmware for $RTOS platform $PLATFORM"
-    st-flash --reset write build/nucleof446ze_uros_template.bin 0x8000000
+    if [$USE_STFLASH = true]; then 
+      st-flash --reset write build/micro-ROS.bin 0x8000000
+    else
+      if lsusb -d 0483:374b; then
+        ST_INTERFACE=interface/stlink-v2-1.cfg
+      elif lsusb -d 0483:3748; then
+        ST_INTERFACE=interface/stlink-v2.cfg
+      else
+      # TODO: add stlink v3, should it be stlink.cfg ?
+        echo "Error. Unsuported OpenOCD USB programmer"
+        exit 1
+      fi
+      openocd -f $ST_INTERFACE -f target/stm32f4x.cfg -c init -c "reset halt" -c "flash write_image erase build/micro-ROS.bin 0x08000000" -c "reset" -c "exit"
+    fi
   else
-    echo "build/nucleof446ze_uros_template.bin not found: please compile before flashing."
+    echo "build/micro-ROS.bin not found: please compile before flashing."
   fi
 
 popd > /dev/null
