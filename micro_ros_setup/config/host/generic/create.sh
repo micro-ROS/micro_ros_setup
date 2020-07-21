be1ac430291574998c609407be0f2e09545a3fb0
Pablo Garrido <pablogs9@gmail.com>
2020-07-21 16:21:53 +0200

    Fix host creation

--- micro_ros_setup/config/host/generic/create.sh
+++ micro_ros_setup/config/host/generic/create.sh
@@ -6,4 +6,4 @@ cp $PREFIX/config/$RTOS/$PLATFORM/client-host-colcon.meta src/colcon.meta
 
 rosdep install -y --from-paths src -i src --skip-keys="$SKIP"
 
-touch mcu_ws/uros/rclc/rclc_examples/COLCON_IGNORE
+touch src/uros/rclc/rclc_examples/COLCON_IGNORE
