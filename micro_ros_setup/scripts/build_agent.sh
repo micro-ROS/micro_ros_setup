722d0361f9289323d107fd788952c783c9f2e2ba
Jose Antonio Moral <joseantoniomoralparras@gmail.com>
2020-07-24 11:34:45 +0200

    Add specific script to build micro-ROS-Agent (#159)
    * Add specific script to build micro-ROS-Agent
    
    * Use new build_agent.sh in CI
    
    * Use --merge-install option.
    
    * Fix positional arguments

---
+++ micro_ros_setup/scripts/build_agent.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+set -e
+set -o nounset
+set -o pipefail
+
+echo "Building micro-ROS Agent"
+
+colcon build $@ --cmake-args \
+    "-DUAGENT_BUILD_EXECUTABLE=OFF" \
+    "--no-warn-unused-cli"
