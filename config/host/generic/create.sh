1aa4c4ec20afac92e5380b1ab1392ae42ddaec06
Pablo Garrido <pablogs9@gmail.com>
2020-08-28 09:09:20 +0200

    Fix Micro-ROS host dependencies mix with ROS2 (#185)
    Update
    
    
    Update
    
    
    update
    
    
    Added packages file
    
    
    Updating CI 
    
    
    Update CI
    
    
    Update CI
    
    
    Update CI
    
    
    Update CI
    
    
    Last one
    
    
    Update agent

--- config/host/generic/create.sh
+++ config/host/generic/create.sh
@@ -1,5 +1,5 @@
 # populate the workspace
-ros2 run micro_ros_setup create_ws.sh src $PREFIX/config/client_ros2_packages.txt $PREFIX/config/$RTOS/$PLATFORM/client_host_packages.repos
+ros2 run micro_ros_setup create_ws.sh src $PREFIX/config/$RTOS/client_ros2_packages.txt $PREFIX/config/$RTOS/$PLATFORM/client_host_packages.repos
 
 # add appropriate colcon.meta
 cp $PREFIX/config/$RTOS/$PLATFORM/client-host-colcon.meta src/colcon.meta
