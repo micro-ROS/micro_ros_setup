ad4092eaf97b7325a11bfdd41ef8444eecd4262c
Pablo Garrido <pablogs9@gmail.com>
2020-09-01 08:45:26 +0200

    Added lines to build typesupport earlier

--- config/host/generic/build.sh
+++ config/host/generic/build.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+colcon build --packages-up-to rosidl_typesupport_microxrcedds_c --metas src $@
+colcon build --packages-up-to rosidl_typesupport_microxrcedds_cpp --metas src $@
 colcon build --metas src $@
