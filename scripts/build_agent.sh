2f59d8704ff1ecec2f53d6e4aa514766d6b26773
Pablo Garrido <pablogs9@gmail.com>
2020-10-13 09:29:45 +0200

    Disable XRCE P2P Agent for micro-ROS (#211)

--- scripts/build_agent.sh
+++ scripts/build_agent.sh
@@ -6,6 +6,7 @@ set -o pipefail
 
 echo "Building micro-ROS Agent"
 
-colcon build $@ --cmake-args \
+colcon build --packages-select micro_ros_agent microxrcedds_agent drive_base_msgs $@ --cmake-args \
     "-DUAGENT_BUILD_EXECUTABLE=OFF" \
+    "-DUAGENT_P2P_PROFILE=OFF" \
     "--no-warn-unused-cli"
