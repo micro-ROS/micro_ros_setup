c534b5691424e4d438c52c8340709213e4f10b1d
Esteve Fernandez <esteve@apache.org>
2020-10-19 12:15:17 +0200

    Added initial support for zenoh-pico

---
+++ config/host-zenoh-pico/generic/create.sh
@@ -0,0 +1,10 @@
+# populate the workspace
+ros2 run micro_ros_setup create_ws.sh src $PREFIX/config/$RTOS/client_ros2_packages.txt $PREFIX/config/$RTOS/$PLATFORM/client_host_packages.repos
+
+# add appropriate colcon.meta
+cp $PREFIX/config/$RTOS/$PLATFORM/client-host-colcon.meta src/colcon.meta
+
+rosdep install -y --from-paths src -i src --skip-keys="$SKIP"
+
+touch src/uros/rclc/rclc_examples/COLCON_IGNORE
+touch src/uros/rclc/rclc_lifecycle/COLCON_IGNORE
