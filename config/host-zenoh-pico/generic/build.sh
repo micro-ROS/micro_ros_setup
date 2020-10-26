a8254fbbc697886549c18fc0215ec873411fb7e1
Esteve Fernandez <esteve@apache.org>
2020-10-26 11:08:19 +0100

    Ignore, not skip

--- config/host-zenoh-pico/generic/build.sh
+++ config/host-zenoh-pico/generic/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-colcon build --packages-up-to rosidl_typesupport_zenoh_c --packages-skip zenoh_vendor --metas src $@
-colcon build --packages-up-to rosidl_typesupport_zenoh_cpp --packages-skip zenoh_vendor --metas src $@
+colcon build --packages-up-to rosidl_typesupport_zenoh_c --packages-ignore zenoh_vendor --metas src $@
+colcon build --packages-up-to rosidl_typesupport_zenoh_cpp --packages-ignore zenoh_vendor --metas src $@
 
 set +o nounset
 . /opt/ros/$ROS_DISTRO/setup.bash
 . install/local_setup.bash
 set -o nounset
 
-colcon build --packages-skip zenoh_vendor --metas src $@
+colcon build --packages-ignore zenoh_vendor --metas src $@
