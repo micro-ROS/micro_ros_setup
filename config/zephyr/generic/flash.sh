2d3262cc283fa19100d679b18164869edff1bc57
Robert Wilbrandt <robert@stamm-wilbrandt.de>
2020-11-08 18:10:16 +0100

    Unify zephyr flash openocd handling
    This changes the default behavior of the olimex-stm32-e407 platform,
    which previously didn't run directly after flashing. I could not find a
    reason why this should be the case and thus made it consistent with the
    "west flash" and nucleo_h743zi behavior.
    
    Signed-off-by: Robert Wilbrandt <robert@stamm-wilbrandt.de>

--- config/zephyr/generic/flash.sh
+++ config/zephyr/generic/flash.sh
@@ -21,8 +21,48 @@ else
 
 
 
-    # Choose flash method based on board
-    if [ "$PLATFORM" = "discovery_l475_iot1" ] || [ "$PLATFORM" = "nucleo_f401re" ]; then
+    # These boards need special openocd rules
+    FLASH_OPENOCD=false
+    if [ "$PLATFORM" = "olimex-stm32-e407" ]; then
+
+        FLASH_OPENOCD=true
+        OPENOCD_TARGET="stm32f4x.cfg"
+        if lsusb -d 15BA:002a; then
+            OPENOCD_PROGRAMMER=interface/ftdi/olimex-arm-usb-tiny-h.cfg
+        elif lsusb -d 15BA:0003;then
+            OPENOCD_PROGRAMMER=interface/ftdi/olimex-arm-usb-ocd.cfg
+        elif lsusb -d 15BA:002b;then
+            OPENOCD_PROGRAMMER=interface/ftdi/olimex-arm-usb-ocd-h.cfg
+        else
+            echo "Error: Unsuported OpenOCD USB programmer"
+            exit 1
+        fi
+
+    elif [ "$PLATFORM" = "nucleo_h743zi" ]; then
+
+        FLASH_OPENOCD=true
+        OPENOCD_TARGET="stm32h7x.cfg"
+
+        if lsusb -d 0483:374e;then
+            OPENOCD_PROGRAMMER=interface/stlink.cfg
+        else
+            echo "Error: Unsupported OpenOCD programmer"
+            exit 1
+        fi
+
+    fi
+
+
+
+    if [ "$FLASH_OPENOCD" = true ]; then
+
+        openocd -f $OPENOCD_PROGRAMMER -f target/$OPENOCD_TARGET \
+                -c init \
+                -c "reset halt" \
+                -c "flash write_image erase $ZEPHYR_BUILD_DIR/zephyr.bin 0x08000000" \
+                -c "reset run; exit"
+
+    elif [ "$PLATFORM" = "discovery_l475_iot1" ] || [ "$PLATFORM" = "nucleo_f401re" ]; then
 
         export ZEPHYR_TOOLCHAIN_VARIANT=zephyr
         export ZEPHYR_SDK_INSTALL_DIR=$FW_TARGETDIR/zephyr-sdk
@@ -32,33 +72,6 @@ else
         export PATH=~/.local/bin:"$PATH"
 
         west flash
-
-    elif [ "$PLATFORM" = "olimex-stm32-e407" ]; then
-
-        if lsusb -d 15BA:002a; then
-            PROGRAMMER=interface/ftdi/olimex-arm-usb-tiny-h.cfg
-        elif lsusb -d 15BA:0003;then
-            PROGRAMMER=interface/ftdi/olimex-arm-usb-ocd.cfg
-        elif lsusb -d 15BA:002b;then
-            PROGRAMMER=interface/ftdi/olimex-arm-usb-ocd-h.cfg
-        else
-            echo "Error: Unsuported OpenOCD USB programmer"
-            exit 1
-        fi
-
-        openocd -f $PROGRAMMER -f target/stm32f4x.cfg -c init -c "reset halt" -c "flash write_image erase build/zephyr/zephyr.bin 0x08000000" -c "reset" -c "exit"
-
-    elif [ "$PLATFORM" = "nucleo_h743zi" ]; then
-
-        if lsusb -d 0483:374e;then
-            PROGRAMMER=interface/stlink.cfg
-            TARGET=stm32h7x.cfg
-        else
-            echo "Error: Unsupported OpenOCD programmer"
-            exit 1
-        fi
-
-        openocd -f $PROGRAMMER -f target/$TARGET -c init -c "reset halt" -c "flash write_image erase build/zephyr/zephyr.bin 0x08000000" -c "reset run; exit"
 
     else
 
