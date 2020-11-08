66e2686fdc776c870746b15032e4c11fea90c0b8
Robert Wilbrandt <robert@stamm-wilbrandt.de>
2020-11-08 17:33:51 +0100

    Clean up zephyr flash step
    There were some issues here:
    - The handling of the nucleo_h743zi platform seems to have been broken
      (caused by a wrong nesting in the platform selection)
    - Not all cases checked for the build artifact before using it
    - Console output differed between different platforms
    - Indentation was inconsistent
    
    Signed-off-by: Robert Wilbrandt <robert@stamm-wilbrandt.de>

--- config/zephyr/generic/flash.sh
+++ config/zephyr/generic/flash.sh
@@ -1,48 +1,72 @@
 pushd $FW_TARGETDIR > /dev/null
 
-if [ "$PLATFORM" = "discovery_l475_iot1" ] || [ "$PLATFORM" = "nucleo_f401re" ]; then
-  export ZEPHYR_TOOLCHAIN_VARIANT=zephyr
-  export ZEPHYR_SDK_INSTALL_DIR=$FW_TARGETDIR/zephyr-sdk
+ZEPHYR_BUILD_DIR="$FW_TARGETDIR/build/zephyr"
 
-  source $FW_TARGETDIR/zephyrproject/zephyr/zephyr-env.sh
+# Host platform (=native_posix) is special, as flashing is actually just executing the binary
+if [ "$PLATFORM" = "host" ]; then
 
-  export PATH=~/.local/bin:"$PATH"
-
-  west flash
-elif [ "$PLATFORM" = "olimex-stm32-e407" ]; then
-  if [ -f build/zephyr/zephyr.bin ]; then
-    echo "Flashing firmware for $RTOS platform $PLATFORM"
-
-      if lsusb -d 15BA:002a; then
-        PROGRAMMER=interface/ftdi/olimex-arm-usb-tiny-h.cfg
-      elif lsusb -d 15BA:0003;then
-        PROGRAMMER=interface/ftdi/olimex-arm-usb-ocd.cfg
-      elif lsusb -d 15BA:002b;then
-        PROGRAMMER=interface/ftdi/olimex-arm-usb-ocd-h.cfg
-      else
-        echo "Error. Unsuported OpenOCD USB programmer"
+    if [ ! -f "$ZEPHYR_BUILD_DIR/zephyr.exe" ]; then
+        echo "Error: $ZEPHYR_BUILD_DIR/zephyr.exe not found. Please compile before flashing."
         exit 1
-      fi
-
-      openocd -f $PROGRAMMER -f target/stm32f4x.cfg -c init -c "reset halt" -c "flash write_image erase build/zephyr/zephyr.bin 0x08000000" -c "reset" -c "exit"
-
-  elif [ "$PLATFORM" = "nucleo_h743zi" ]; then
-    if lsusb -d 0483:374e;then
-        PROGRAMMER=interface/stlink.cfg
-        TARGET=stm32h7x.cfg
     fi
 
-    openocd -f $PROGRAMMER -f target/$TARGET -c init -c "reset halt" -c "flash write_image erase build/zephyr/zephyr.bin 0x08000000" -c "reset run; exit"
+    $ZEPHYR_BUILD_DIR/zephyr.exe
 
-  else
-    echo "build/zephyr/zephyr.bin not found: please compile before flashing."
-  fi
-elif [ "$PLATFORM" = "host" ]; then
-  ./build/zephyr/zephyr.exe 
 else
-  echo "Unrecognized board: $PLATFORM"
-  exit 1
+
+    if [ ! -f "$ZEPHYR_BUILD_DIR/zephyr.bin" ]; then
+        echo "Error: $ZEPHYR_BUILD_DIR/zephyr.bin not found. Please compile before flashing."
+        exit 1
+    fi
+
+
+
+    # Choose flash method based on board
+    if [ "$PLATFORM" = "discovery_l475_iot1" ] || [ "$PLATFORM" = "nucleo_f401re" ]; then
+
+        export ZEPHYR_TOOLCHAIN_VARIANT=zephyr
+        export ZEPHYR_SDK_INSTALL_DIR=$FW_TARGETDIR/zephyr-sdk
+
+        source $FW_TARGETDIR/zephyrproject/zephyr/zephyr-env.sh
+
+        export PATH=~/.local/bin:"$PATH"
+
+        west flash
+
+    elif [ "$PLATFORM" = "olimex-stm32-e407" ]; then
+
+        if lsusb -d 15BA:002a; then
+            PROGRAMMER=interface/ftdi/olimex-arm-usb-tiny-h.cfg
+        elif lsusb -d 15BA:0003;then
+            PROGRAMMER=interface/ftdi/olimex-arm-usb-ocd.cfg
+        elif lsusb -d 15BA:002b;then
+            PROGRAMMER=interface/ftdi/olimex-arm-usb-ocd-h.cfg
+        else
+            echo "Error: Unsuported OpenOCD USB programmer"
+            exit 1
+        fi
+
+        openocd -f $PROGRAMMER -f target/stm32f4x.cfg -c init -c "reset halt" -c "flash write_image erase build/zephyr/zephyr.bin 0x08000000" -c "reset" -c "exit"
+
+    elif [ "$PLATFORM" = "nucleo_h743zi" ]; then
+
+        if lsusb -d 0483:374e;then
+            PROGRAMMER=interface/stlink.cfg
+            TARGET=stm32h7x.cfg
+        else
+            echo "Error: Unsupported OpenOCD programmer"
+            exit 1
+        fi
+
+        openocd -f $PROGRAMMER -f target/$TARGET -c init -c "reset halt" -c "flash write_image erase build/zephyr/zephyr.bin 0x08000000" -c "reset run; exit"
+
+    else
+
+        echo "Unrecognized board: $PLATFORM"
+        exit 1
+
+    fi
+
 fi
 
 popd > /dev/null
-
