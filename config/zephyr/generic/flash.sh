0cac790119a8aaec0bfe1dcbca43d9b70ea46fe5
RobertWilbrandt <robert@stamm-wilbrandt.de>
2020-11-10 07:18:05 +0100

    Improve flash step for zephyr RTOS (#225)
    * Clean up zephyr flash step
    
    There were some issues here:
    - The handling of the nucleo_h743zi platform seems to have been broken
      (caused by a wrong nesting in the platform selection)
    - Not all cases checked for the build artifact before using it
    - Console output differed between different platforms
    - Indentation was inconsistent
    
    Signed-off-by: Robert Wilbrandt <robert@stamm-wilbrandt.de>
    
    * Unify zephyr flash openocd handling
    
    This changes the default behavior of the olimex-stm32-e407 platform,
    which previously didn't run directly after flashing. I could not find a
    reason why this should be the case and thus made it consistent with the
    "west flash" and nucleo_h743zi behavior.
    
    Signed-off-by: Robert Wilbrandt <robert@stamm-wilbrandt.de>
    
    * Default to "west flash" when flashing with zephyr
    
    There is no real reason to check for platform validity there, and as we
    use zephyr this seems like a sane default. This also allows the usage of
    any zephyr-supported board (which can actually run uros) by just adding
    its name to the supported_platforms file (previously, a special casing
    in flash.sh was also needed).
    
    Signed-off-by: Robert Wilbrandt <robert@stamm-wilbrandt.de>

--- config/zephyr/generic/flash.sh
+++ config/zephyr/generic/flash.sh
@@ -1,48 +1,79 @@
 pushd $FW_TARGETDIR > /dev/null
 
-if [ "$PLATFORM" = "discovery_l475_iot1" ] || [ "$PLATFORM" = "nucleo_f401re" ]; then
-  export ZEPHYR_TOOLCHAIN_VARIANT=zephyr
-  export ZEPHYR_SDK_INSTALL_DIR=$FW_TARGETDIR/zephyr-sdk
+ZEPHYR_BUILD_DIR="$FW_TARGETDIR/build/zephyr"
 
-  source $FW_TARGETDIR/zephyrproject/zephyr/zephyr-env.sh
+# Host platform (=native_posix) is special, as flashing is actually just executing the binary
+if [ "$PLATFORM" = "host" ]; then
 
-  export PATH=~/.local/bin:"$PATH"
-
-  west flash
-elif [ "$PLATFORM" = "olimex-stm32-e407" ]; then
-  if [ -f build/zephyr/zephyr.bin ]; then
-    echo "Flashing firmware for $RTOS platform $PLATFORM"
-
-      if lsusb -d 15BA:002a; then
-        PROGRAMMER=interface/ftdi/olimex-arm-usb-tiny-h.cfg
-      elif lsusb -d 15BA:0003;then
-        PROGRAMMER=interface/ftdi/olimex-arm-usb-ocd.cfg
-      elif lsusb -d 15BA:002b;then
-        PROGRAMMER=interface/ftdi/olimex-arm-usb-ocd-h.cfg
-      else
-        echo "Error. Unsuported OpenOCD USB programmer"
+    if [ ! -f "$ZEPHYR_BUILD_DIR/zephyr.exe" ]; then
+        echo "Error: $ZEPHYR_BUILD_DIR/zephyr.exe not found. Please compile before flashing."
         exit 1
-      fi
-
-      openocd -f $PROGRAMMER -f target/stm32f4x.cfg -c init -c "reset halt" -c "flash write_image erase build/zephyr/zephyr.bin 0x08000000" -c "reset" -c "exit"
-
-  elif [ "$PLATFORM" = "nucleo_h743zi" ]; then
-    if lsusb -d 0483:374e;then
-        PROGRAMMER=interface/stlink.cfg
-        TARGET=stm32h7x.cfg
     fi
 
-    openocd -f $PROGRAMMER -f target/$TARGET -c init -c "reset halt" -c "flash write_image erase build/zephyr/zephyr.bin 0x08000000" -c "reset run; exit"
+    $ZEPHYR_BUILD_DIR/zephyr.exe
 
-  else
-    echo "build/zephyr/zephyr.bin not found: please compile before flashing."
-  fi
-elif [ "$PLATFORM" = "host" ]; then
-  ./build/zephyr/zephyr.exe 
 else
-  echo "Unrecognized board: $PLATFORM"
-  exit 1
+
+    if [ ! -f "$ZEPHYR_BUILD_DIR/zephyr.bin" ]; then
+        echo "Error: $ZEPHYR_BUILD_DIR/zephyr.bin not found. Please compile before flashing."
+        exit 1
+    fi
+
+
+
+    # These boards need special openocd rules
+    FLASH_OPENOCD=false
+    if [ "$PLATFORM" = "olimex-stm32-e407" ]; then
+
+        FLASH_OPENOCD=true
+        OPENOCD_TARGET="stm32f4x.cfg"
+        if lsusb -d 15BA:002a; then
+            OPENOCD_PROGRAMMER=interface/ftdi/olimex-arm-usb-tiny-h.cfg
+        elif lsusb -d 15BA:0003;then
+            OPENOCD_PROGRAMMER=interface/ftdi/olimex-arm-usb-ocd.cfg
+        elif lsusb -d 15BA:002b;then
+            OPENOCD_PROGRAMMER=interface/ftdi/olimex-arm-usb-ocd-h.cfg
+        else
+            echo "Error: Unsuported OpenOCD USB programmer"
+            exit 1
+        fi
+
+    elif [ "$PLATFORM" = "nucleo_h743zi" ]; then
+
+        FLASH_OPENOCD=true
+        OPENOCD_TARGET="stm32h7x.cfg"
+
+        if lsusb -d 0483:374e;then
+            OPENOCD_PROGRAMMER=interface/stlink.cfg
+        else
+            echo "Error: Unsupported OpenOCD programmer"
+            exit 1
+        fi
+
+    fi
+
+
+
+    if [ "$FLASH_OPENOCD" = true ]; then
+
+        openocd -f $OPENOCD_PROGRAMMER -f target/$OPENOCD_TARGET \
+                -c init \
+                -c "reset halt" \
+                -c "flash write_image erase $ZEPHYR_BUILD_DIR/zephyr.bin 0x08000000" \
+                -c "reset run; exit"
+
+    else
+
+        export ZEPHYR_TOOLCHAIN_VARIANT=zephyr
+        export ZEPHYR_SDK_INSTALL_DIR=$FW_TARGETDIR/zephyr-sdk
+        export PATH=~/.local/bin:"$PATH"
+
+        source $FW_TARGETDIR/zephyrproject/zephyr/zephyr-env.sh
+
+        west flash
+
+    fi
+
 fi
 
 popd > /dev/null
-
