4ac4a45e8f5138e9f1dd4b3d2beff0a1072dbb80
Jose Antonio Moral <joseantoniomoralparras@gmail.com>
2020-11-16 16:50:39 +0100

    Improve build_agent.sh script (#232)

--- scripts/build_agent.sh
+++ scripts/build_agent.sh
@@ -6,7 +6,7 @@ set -o pipefail
 
 echo "Building micro-ROS Agent"
 
-colcon build --packages-select micro_ros_agent microxrcedds_agent drive_base_msgs $@ --cmake-args \
+colcon build --packages-up-to micro_ros_agent $@ --cmake-args \
     "-DUAGENT_BUILD_EXECUTABLE=OFF" \
     "-DUAGENT_P2P_PROFILE=OFF" \
     "--no-warn-unused-cli"
