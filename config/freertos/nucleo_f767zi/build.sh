b6786a054fc75bb068eaf12d2790b753054fad76
James Nugen <jnugen@gmail.com>
2020-12-08 02:25:15 -0800

    Add port for Nucleo-F767ZI

---
+++ config/freertos/nucleo_f767zi/build.sh
@@ -0,0 +1,36 @@
+EXTENSIONS_DIR=$FW_TARGETDIR/freertos_apps/microros_nucleo_f767zi_extensions
+
+. $PREFIX/config/utils.sh
+
+pushd $EXTENSIONS_DIR >/dev/null
+
+    export UROS_APP=$(head -n1 $FW_TARGETDIR/APP | tail -n1)
+
+    if [ -v UROS_CUSTOM_APP_FOLDER ]; then
+        export UROS_APP_FOLDER="$UROS_CUSTOM_APP_FOLDER/$UROS_APP"
+    else
+        export UROS_APP_FOLDER="$FW_TARGETDIR/freertos_apps/apps/$UROS_APP"
+    fi
+
+    if [ -d "$UROS_APP_FOLDER" ]; then
+        echo "Selected app: $UROS_APP"
+    else
+        echo "App not found: $UROS_APP"
+        print_available_apps
+        exit 1
+    fi
+
+    if [ "$UROS_FAST_BUILD" = "off" ] || [ ! -d "build" ]; then
+        # Clean micro-ROS build
+        rm -rf $FW_TARGETDIR/mcu_ws/build $FW_TARGETDIR/mcu_ws/install $FW_TARGETDIR/mcu_ws/log
+
+        # Clean build
+        make clean
+
+        # Build micro-ROS stack
+        make libmicroros
+    fi
+
+    # Build firmware
+    make -j$(nproc) UROS_APP_FOLDER=$UROS_APP_FOLDER
+popd >/dev/null
