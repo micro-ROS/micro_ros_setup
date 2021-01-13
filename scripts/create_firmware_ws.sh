7ce311487798f5342b64047c5ec581b3f045630b
Pablo Garrido <pablogs9@gmail.com>
2021-01-13 10:28:27 +0100

    Initial renaming

--- scripts/create_firmware_ws.sh
+++ scripts/create_firmware_ws.sh
@@ -72,7 +72,7 @@ SKIP="microxrcedds_agent microxrcedds_client microcdr rosidl_typesupport_connext
 
 # Installing common packages 
 rosdep update
-rosdep install -y --from-paths src -i src --rosdistro foxy --skip-keys="$SKIP"
+rosdep install -y --from-paths src -i src --rosdistro $ROS_DISTRO --skip-keys="$SKIP"
 
 # Check generic build
 if [ $PLATFORM != "generic" ] && [ -d "$PREFIX/config/$RTOS/generic" ]; then
@@ -88,7 +88,7 @@ pushd $FW_TARGETDIR >/dev/null
     if [ $RTOS != "host" ]; then
         ros2 run micro_ros_setup create_ws.sh $DEV_WS_DIR $PREFIX/config/$RTOS/dev_ros2_packages.txt \
             $PREFIX/config/$RTOS/dev_uros_packages.repos
-        rosdep install -y --from-paths $DEV_WS_DIR -i $DEV_WS_DIR --rosdistro foxy --skip-keys="$SKIP"
+        rosdep install -y --from-paths $DEV_WS_DIR -i $DEV_WS_DIR --rosdistro $ROS_DISTRO --skip-keys="$SKIP"
 
          # Creating mcu directory
         mkdir mcu_ws
@@ -109,7 +109,7 @@ if [ $RTOS != "host" ]; then
 fi
 
 # Install dependecies for specific platform
-rosdep install -y --from-paths $PREFIX/config/$RTOS/$TARGET_FOLDER -i $PREFIX/config/$RTOS/$TARGET_FOLDER --rosdistro foxy --skip-keys="$SKIP"
+rosdep install -y --from-paths $PREFIX/config/$RTOS/$TARGET_FOLDER -i $PREFIX/config/$RTOS/$TARGET_FOLDER --rosdistro $ROS_DISTRO --skip-keys="$SKIP"
 
 # Creating specific firmware folder
 . $PREFIX/config/$RTOS/$TARGET_FOLDER/create.sh
