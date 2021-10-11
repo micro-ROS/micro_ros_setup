967e7ad8e0993312d0c92eb4ff8fe28a9a243d70
kneep <alvin.cao@gmail.com>
2021-10-11 21:10:42 +0800

    Add android support config (#396)
    Signed-off-by: Joel Cao <joel.cao@oppo.com>
    
    Co-authored-by: Joel Cao <joel.cao@oppo.com>

---
+++ config/android/generic/create.sh
@@ -0,0 +1,11 @@
+pushd $FW_TARGETDIR >/dev/null
+
+    # ignore broken packages
+    touch mcu_ws/ros2/rcl_logging/rcl_logging_log4cxx/COLCON_IGNORE
+    touch mcu_ws/ros2/rcl_logging/rcl_logging_spdlog/COLCON_IGNORE
+    touch mcu_ws/ros2/rcl/COLCON_IGNORE
+    touch mcu_ws/ros2/rosidl/rosidl_typesupport_introspection_cpp/COLCON_IGNORE
+    touch mcu_ws/uros/rcl/rcl_yaml_param_parser/COLCON_IGNORE
+    touch mcu_ws/uros/rclc/rclc_examples/COLCON_IGNORE
+
+popd >/dev/null
