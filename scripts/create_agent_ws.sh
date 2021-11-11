ffaf3863d7010ac39cac05696ad7558a7c09b120
Pablo Garrido <pablogs9@gmail.com>
2021-11-11 10:43:04 +0100

    Fix rosdep

--- scripts/create_agent_ws.sh
+++ scripts/create_agent_ws.sh
@@ -6,7 +6,7 @@ set -o pipefail
 
 PREFIX=$(ros2 pkg prefix micro_ros_setup)
 TARGETDIR=src
-SKIP="rosidl_typesupport_opensplice_c rosidl_typesupport_opensplice_cpp rmw_opensplice_cpp rmw_connext_cpp rosidl_typesupport_connext_c rosidl_typesupport_connext_cpp microxrcedds_agent microxrcedds_client microcdr"
+SKIP="rosidl_typesupport_opensplice_c rosidl_typesupport_opensplice_cpp rmw_opensplice_cpp rmw_connext_cpp rosidl_typesupport_connext_c rosidl_typesupport_connext_cpp microxrcedds_agent microxrcedds_client microcdr rmw_connextdds $EXTERNAL_SKIP"
 
 if [ $# -gt 0 ]
 then
@@ -18,5 +18,4 @@ fi
 # populate the workspace
 ros2 run micro_ros_setup create_ws.sh $TARGETDIR $PREFIX/config/agent_ros2_packages.txt $PREFIX/config/agent_uros_packages.repos
 
-rosdep install --from-paths $TARGETDIR -i $TARGETDIR -y \
-  --skip-keys="$SKIP"
+rosdep install --from-paths $TARGETDIR -i $TARGETDIR -y --skip-keys="$SKIP"
