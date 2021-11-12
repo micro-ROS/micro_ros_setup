542b3e6152f0120e0891f62571ebca1fcef30c71
Pablo Garrido <pablogs9@gmail.com>
2021-11-12 12:14:29 +0100

    Modify host build

--- config/host/generic/build.sh
+++ config/host/generic/build.sh
@@ -3,7 +3,6 @@ colcon build --packages-up-to rosidl_typesupport_microxrcedds_c --metas src --cm
 colcon build --packages-up-to rosidl_typesupport_microxrcedds_cpp --metas src --cmake-args -DBUILD_TESTING=OFF $@
 
 set +o nounset
-. /opt/ros/$ROS_DISTRO/setup.bash
 . install/local_setup.bash
 set -o nounset
 
