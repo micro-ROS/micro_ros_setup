73695afda5edcda4e4a52956e657e41759f5d6a3
Pablo Garrido <pablogs9@gmail.com>
2022-03-02 16:31:42 +0100

    Fix mbed (#462)
    Signed-off-by: Pablo Garrido <pablogs9@gmail.com>
    (cherry picked from commit 5485882978ad66542f4b40c1e28cbb73a9cfee9d)

--- config/mbed/generic/build.sh
+++ config/mbed/generic/build.sh
@@ -3,7 +3,7 @@
 export PATH=~/.local/bin:"$PATH"
 
 pushd $FW_TARGETDIR >/dev/null
-    
+
     pushd micro_ros_mbed >/dev/null
         mbed-tools compile -m $PLATFORM -t GCC_ARM
     popd >/dev/null
