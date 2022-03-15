f64f6f88e0ce80db51196421bb9eb4f0bcdbc33b
VictorLee <lzs_1993@qq.com>
2022-03-15 15:02:26 +0800

    build dynamic lib of rosidl_typesupport of (#473)
    microxrcedds_c
    
    (cherry picked from commit 3136ad07d98cc4b50badfc57620325b5dc2b55eb)
    
    # Conflicts:
    #	config/host/generic/build.sh
    #	config/host/generic/client_host_packages.repos

--- config/host/generic/build.sh
+++ config/host/generic/build.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+<<<<<<< HEAD
 colcon build --packages-up-to rosidl_typesupport_microxrcedds_c --metas src $@
 colcon build --packages-up-to rosidl_typesupport_microxrcedds_cpp --metas src $@
+=======
+colcon build --packages-up-to rosidl_typesupport_microxrcedds_c --metas src --cmake-args -DBUILD_TESTING=OFF -DBUILD_SHARED_LIBS=ON $@
+colcon build --packages-up-to rosidl_typesupport_microxrcedds_cpp --metas src --cmake-args -DBUILD_TESTING=OFF -DBUILD_SHARED_LIBS=ON $@
+>>>>>>> 3136ad0 (build dynamic lib of rosidl_typesupport of (#473))
 
 set +o nounset
 . install/local_setup.bash
 set -o nounset
 
+<<<<<<< HEAD
 colcon build --metas src $@
+=======
+colcon build --metas src --cmake-args -DBUILD_TESTING=OFF -DBUILD_SHARED_LIBS=ON $@
+>>>>>>> 3136ad0 (build dynamic lib of rosidl_typesupport of (#473))
