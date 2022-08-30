f35e95c15a44f5ac1c1ef26d1a884e79c2eb8658
gavanderhoorn <g.a.vanderhoorn@tudelft.nl>
2022-08-30 13:12:49 +0200

    All indents to tabs (build.sh)
    Was a mix of tabs and spaces for indents.
    
    Signed-off-by: gavanderhoorn <g.a.vanderhoorn@tudelft.nl>

--- config/generate_lib/generic/build.sh
+++ config/generate_lib/generic/build.sh
@@ -1,18 +1,18 @@
 . $PREFIX/config/utils.sh
 
 if [ $# -ge 1 ]; then
-    TOOLCHAIN=$1
+	TOOLCHAIN=$1
 else
-    echo "Syntax: ros2 run micro_ros_setup build_firmware.sh <CMake toolchain file> [Colcon meta file]"
-    exit 1
+	echo "Syntax: ros2 run micro_ros_setup build_firmware.sh <CMake toolchain file> [Colcon meta file]"
+	exit 1
 fi
 
 if [ $# -ge 2 ]; then
-    COLCON_META=$2
+	COLCON_META=$2
 	echo "Using provided meta: $COLCON_META"
 
 else
-    COLCON_META=$FW_TARGETDIR/mcu_ws/colcon.meta
+	COLCON_META=$FW_TARGETDIR/mcu_ws/colcon.meta
 	echo "Using default meta: $COLCON_META"
 fi
 
@@ -23,7 +23,7 @@ pushd $FW_TARGETDIR/mcu_ws >/dev/null
 
 	rm -rf build install log
 	
-   	colcon build \
+	colcon build \
 		--merge-install \
 		--packages-ignore-regex=.*_cpp \
 		--metas $COLCON_META \
@@ -37,7 +37,7 @@ pushd $FW_TARGETDIR/mcu_ws >/dev/null
 		-DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN \
 		-DCMAKE_VERBOSE_MAKEFILE=ON; \
 
-    mkdir -p $FW_TARGETDIR/libmicroros; cd $FW_TARGETDIR/libmicroros; \
+	mkdir -p $FW_TARGETDIR/libmicroros; cd $FW_TARGETDIR/libmicroros; \
 	echo "CREATE libmicroros.a" > $FW_TARGETDIR/libmicroros/ar_script.m; \
 	for file in $(find "$FW_TARGETDIR/install/lib/" -name '*.a'); do \
 		echo "ADDLIB ${file}" >> $FW_TARGETDIR/libmicroros/ar_script.m; \
@@ -46,7 +46,7 @@ pushd $FW_TARGETDIR/mcu_ws >/dev/null
 	echo "END" >> $FW_TARGETDIR/libmicroros/ar_script.m; \
 	ar -M < $FW_TARGETDIR/libmicroros/ar_script.m; \
 	mkdir -p $BUILD_DIR; cp libmicroros.a $BUILD_DIR; \
-    cp -R $FW_TARGETDIR/mcu_ws/install/include $BUILD_DIR/; \
+	cp -R $FW_TARGETDIR/mcu_ws/install/include $BUILD_DIR/; \
 	cd ..; rm -rf libmicroros;
 
 popd >/dev/null
