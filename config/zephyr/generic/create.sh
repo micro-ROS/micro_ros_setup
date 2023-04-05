39e37adf4195973c65c0f8acd294649fbe1d1488
acuadros95 <acuadros1995@gmail.com>
2023-04-05 09:20:27 +0200

    Remove previous changes
    Signed-off-by: acuadros95 <acuadros1995@gmail.com>

--- config/zephyr/generic/create.sh
+++ config/zephyr/generic/create.sh
@@ -61,13 +61,6 @@ pushd $FW_TARGETDIR >/dev/null
     # ignore broken packages
     touch mcu_ws/ros2/rcl_logging/rcl_logging_spdlog/COLCON_IGNORE
     touch mcu_ws/ros2/rcl/COLCON_IGNORE
-    touch mcu_ws/ros2/rosidl/rosidl_runtime_c/COLCON_IGNORE
-    touch mcu_ws/ros2/rosidl/rosidl_generator_cpp/COLCON_IGNORE
-    touch mcu_ws/ros2/rosidl/rosidl_runtime_cpp/COLCON_IGNORE
-    touch mcu_ws/ros2/rosidl/rosidl_typesupport_interface/COLCON_IGNORE
-    touch mcu_ws/ros2/rosidl/rosidl_typesupport_introspection_c/COLCON_IGNORE
-    touch mcu_ws/ros2/rosidl/rosidl_typesupport_introspection_cpp/COLCON_IGNORE
-    touch mcu_ws/ros2/rosidl/rosidl_typesupport_introspection_tests/COLCON_IGNORE
     touch mcu_ws/ros2/rosidl/rosidl_typesupport_introspection_cpp/COLCON_IGNORE
     touch mcu_ws/ros2/rcpputils/COLCON_IGNORE
     touch mcu_ws/uros/rcl/rcl_yaml_param_parser/COLCON_IGNORE
