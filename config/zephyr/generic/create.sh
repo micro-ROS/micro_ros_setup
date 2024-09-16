8247359a807b884386cfcbcbdad8e2eb445bb72d
Pablo Garrido <pablogs9@gmail.com>
2024-09-16 16:48:20 +0200

    Fix rebase

--- config/zephyr/generic/create.sh
+++ config/zephyr/generic/create.sh
@@ -18,8 +18,6 @@ export PATH=~/.local/bin:"$PATH"
 export ZEPHYR_VERSION="v0.12.4"
 export ARCH=$(uname -m)
 
-<<<<<<< HEAD
-=======
 # Create a virtual environment
 python3 -m venv $FW_TARGETDIR/venv
 source $FW_TARGETDIR/venv/bin/activate
@@ -30,7 +28,6 @@ pip3 install west
 # Install requirements
 pip3 install catkin_pkg empy
 
->>>>>>> 38ae4b7 (Fix broken rosidl_cli (#726))
 pushd $FW_TARGETDIR >/dev/null
 
     west init zephyrproject
