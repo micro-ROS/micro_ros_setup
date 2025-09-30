3c597837ea53205ff6ad9afb146d62181b715fb1
DedSecer <ded5ec@icloud.com>
2025-09-30 16:23:14 +0800

    fix include path (#781)
    * fix include path
    
    * Trigger CI
    
    Signed-off-by: Carlosespicur <carlosespicur@proton.me>
    
    ---------
    
    Signed-off-by: Carlosespicur <carlosespicur@proton.me>
    Co-authored-by: Carlosespicur <carlosespicur@proton.me>
    (cherry picked from commit 22cf5e0068cbd2dd28559cc640da614aa5358b6f)

--- scripts/build_firmware.sh
+++ scripts/build_firmware.sh
@@ -76,3 +76,14 @@ else
     . $PREFIX/config/$RTOS/$PLATFORM/build.sh
 fi
 
+######## Fix include paths  ########
+cd $FW_TARGETDIR/mcu_ws
+INCLUDE_ROS2_PACKAGES=$(colcon list | awk '{print $1}' | awk -v d=" " '{s=(NR==1?s:s d)$0}END{print s}')
+
+for var in ${INCLUDE_ROS2_PACKAGES}; do
+    if [ -d "$FW_TARGETDIR/build/include/${var}/${var}" ]; then
+        rsync -r $FW_TARGETDIR/build/include/${var}/${var}/* $FW_TARGETDIR/build/include/${var}
+        rm -rf $FW_TARGETDIR/build/include/${var}/${var}
+    fi
+done
+
