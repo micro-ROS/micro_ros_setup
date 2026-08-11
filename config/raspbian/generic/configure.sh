461010cfa0ba1eb25c01d58823f8bd83cd61f161
David-LP99 <davidlaseca@eprosima.com>
2026-08-11 11:05:20 +0200

    Fix CI (#826)
    Signed-off-by: David Laseca Perez <davidlaseca@eprosima.com>

--- config/raspbian/generic/configure.sh
+++ config/raspbian/generic/configure.sh
@@ -3,7 +3,8 @@
 pushd $FW_TARGETDIR >/dev/null
     rm -rf mcu_ws/*
     cp raspbian_apps/toolchain.cmake mcu_ws/
-    curl -s https://raw.githubusercontent.com/ros2/ros2/master/ros2.repos |\
+    # ROS_DISTRO SPECIFIC
+    curl -s https://raw.githubusercontent.com/ros2/ros2/kilted/ros2.repos |\
         ros2 run micro_ros_setup yaml_filter.py raspbian_apps/$CONFIG_NAME/ros2_repos.filter > ros2.repos
     vcs import --input ros2.repos mcu_ws/ && rm ros2.repos
 
